acd0d39e2fe734fa072d0d883fdb3f149c6e9a8f
AchinthaBandara <achinthabandara1@gmail.com>
2020-07-30 14:32:08 +0530

    edited sales pop up

--- src/main/webapp/app/Pages/Sales/sales-popup/sales-popup.component.ts
+++ src/main/webapp/app/Pages/Sales/sales-popup/sales-popup.component.ts
@@ -9,46 +9,14 @@ import {PurchaseItems} from "app/shared/model/purchase-items.model";
   styleUrls: ['./sales-popup.component.scss']
 })
 export class SalesPopupComponent implements OnInit {
-  selectedItem: PurchaseItems = new PurchaseItems();
-  total: number;
-  unitPrice: number;
-  units: number;
 
-  message: string;
-
-  constructor(public activeModal: NgbActiveModal, public purchaseData: PurchaseData) {
-    this.total = 0;
-    this.units = 0;
-    this.unitPrice = this.purchaseData.getReviewItem().buyingPrice;
-    this.message = "";
-
+  constructor(public activeModal: NgbActiveModal) {
+    
   }
 
   ngOnInit(): void {
-    this.unitPrice = this.purchaseData.getReviewItem().buyingPrice;
   }
 
-  calculateTotal(): void {
-    this.total = this.units * this.unitPrice;
-  }
 
-  addToCart(): void {
-    this.selectedItem.quantity = this.units;
-    this.selectedItem.total = this.total;
-    this.selectedItem.itemCode = this.purchaseData.reviewItem;
-    this.selectedItem.unitPrice = this.unitPrice;
-    if (this.checkZeroQuantity()) {
-      this.purchaseData.addToCart(this.selectedItem);
-      this.activeModal.close();
-    }
-  }
-
-  checkZeroQuantity(): boolean {
-    if (this.selectedItem.quantity === 0) {
-      this.message = "quantity cannot be 0";
-      return false;
-    }
-    return true;
-  }
 
 }
