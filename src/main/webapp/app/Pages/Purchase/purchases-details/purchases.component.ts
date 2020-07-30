21cf53fec251cca4fbbd6b5d0a03d037214b3a35
AchinthaBandara <achinthabandara1@gmail.com>
2020-07-30 14:29:57 +0530

    edited purchases view, fixed bugs in purchase cart

--- src/main/webapp/app/Pages/Purchase/purchases-details/purchases.component.ts
+++ src/main/webapp/app/Pages/Purchase/purchases-details/purchases.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs';
-import { JhiEventManager, JhiParseLinks } from 'ng-jhipster';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Subscription} from 'rxjs';
+import {JhiEventManager, JhiParseLinks} from 'ng-jhipster';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
-import { IPurchases } from 'app/shared/model/purchases.model';
+import {IPurchases} from 'app/shared/model/purchases.model';
 
-import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
-import { PurchasesService } from './purchases.service';
-import { PurchasesDeleteDialogComponent } from './purchases-delete-dialog.component';
+import {ITEMS_PER_PAGE} from 'app/shared/constants/pagination.constants';
+import {PurchasesService} from './purchases.service';
+import {PurchasesDeleteDialogComponent} from './purchases-delete-dialog.component';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'jhi-purchases',
@@ -23,20 +24,25 @@ export class PurchasesComponent implements OnInit, OnDestroy {
   predicate: string;
   ascending: boolean;
 
+  date: string;
+
+
   constructor(
-    protected purchasesService: PurchasesService,
-    protected eventManager: JhiEventManager,
-    protected modalService: NgbModal,
-    protected parseLinks: JhiParseLinks
-  ) {
-    this.purchases = [];
-    this.itemsPerPage = ITEMS_PER_PAGE;
-    this.page = 0;
-    this.links = {
-      last: 0,
+      protected purchasesService: PurchasesService,
+      protected eventManager: JhiEventManager,
+      protected modalService: NgbModal,
+      protected parseLinks: JhiParseLinks,
+      protected router: Router
+    ) {
+      this.purchases = [];
+      this.itemsPerPage = ITEMS_PER_PAGE;
+      this.page = 0;
+      this.links = {
+        last: 0,
     };
-    this.predicate = 'id';
-    this.ascending = true;
+    this.predicate = 'dateOfPurchase,id';
+    this.ascending = false;
+    this.date = '';
   }
 
   loadAll(): void {
@@ -53,6 +59,7 @@ export class PurchasesComponent implements OnInit, OnDestroy {
     this.page = 0;
     this.purchases = [];
     this.loadAll();
+
   }
 
   loadPage(page: number): void {
@@ -81,8 +88,17 @@ export class PurchasesComponent implements OnInit, OnDestroy {
   }
 
   delete(purchases: IPurchases): void {
-    const modalRef = this.modalService.open(PurchasesDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    const modalRef = this.modalService.open(PurchasesDeleteDialogComponent, {size: 'lg', backdrop: 'static'});
     modalRef.componentInstance.purchases = purchases;
+  }
+
+  getPurchases(): void {
+    this.purchases = [];
+    if (this.date === '') {
+      this.loadAll();
+    } else {
+      this.getPurchasesByDate(this.date);
+    }
   }
 
   sort(): string[] {
@@ -93,6 +109,10 @@ export class PurchasesComponent implements OnInit, OnDestroy {
     return result;
   }
 
+  getPurchasesByDate(date: string): void {
+    this.purchasesService.getPurchasesByDate(date).subscribe((res: HttpResponse<IPurchases[]>) => (this.purchases = res.body || []));
+  }
+
   protected paginatePurchases(data: IPurchases[] | null, headers: HttpHeaders): void {
     const headersLink = headers.get('link');
     this.links = this.parseLinks.parse(headersLink ? headersLink : '');
@@ -101,5 +121,10 @@ export class PurchasesComponent implements OnInit, OnDestroy {
         this.purchases.push(data[i]);
       }
     }
+  }
+
+
+  searchByVendor(): void {
+
   }
 }
