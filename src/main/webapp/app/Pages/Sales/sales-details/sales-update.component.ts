cc18cfb341fd9f1e183c1dbb071700f76a1b27c2
surangi <s16910@sci.pdn.ac.lk>
2020-08-05 21:38:26 +0530

    edited sales details files of the project

--- src/main/webapp/app/Pages/Sales/sales-details/sales-update.component.ts
+++ src/main/webapp/app/Pages/Sales/sales-details/sales-update.component.ts
@@ -4,46 +4,72 @@ import { HttpResponse } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { ICustomerDetails, CustomerDetails } from 'app/shared/model/customer-details.model';
-import { CustomerDetailsService } from './customer-details.service';
-import {SalesData} from "app/Pages/Sales/salesData";
+import { ISales, Sales } from 'app/shared/model/sales.model';
+import { SalesService } from './sales.service';
+import { ICustomerDetails } from 'app/shared/model/customer-details.model';
+import { CustomerDetailsService } from 'app/Pages/Sales/customer-details/customer-details.service';
 @Injectable({providedIn:"root"})
 @Component({
-  selector: 'jhi-customer-details-update',
-  templateUrl: './customer-details-update.component.html',
+  selector: 'jhi-sales-update',
+  templateUrl: './sales-update.component.html',
 })
-export class CustomerDetailsUpdateComponent implements OnInit {
+export class SalesUpdateComponent implements OnInit {
   isSaving = false;
+  customerids: ICustomerDetails[] = [];
+  dateOfSaleDp: any;
 
   editForm = this.fb.group({
     id: [],
-    customerName: [],
-    email: [],
-    address: [],
-    phone: [],
+    total: [],
+    serviceCharges: [],
+    dateOfSale: [],
+    customerID: [],
   });
 
   constructor(
+    protected salesService: SalesService,
     protected customerDetailsService: CustomerDetailsService,
     protected activatedRoute: ActivatedRoute,
-    private fb: FormBuilder,
-    private salesData:SalesData
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ customerDetails }) => {
-      this.updateForm(customerDetails);
+    this.activatedRoute.data.subscribe(({ sales }) => {
+      this.updateForm(sales);
+
+      this.customerDetailsService
+        .query({ filter: 'sales-is-null' })
+        .pipe(
+          map((res: HttpResponse<ICustomerDetails[]>) => {
+            return res.body || [];
+          })
+        )
+        .subscribe((resBody: ICustomerDetails[]) => {
+          if (!sales.customerID || !sales.customerID.id) {
+            this.customerids = resBody;
+          } else {
+            this.customerDetailsService
+              .find(sales.customerID.id)
+              .pipe(
+                map((subRes: HttpResponse<ICustomerDetails>) => {
+                  return subRes.body ? [subRes.body].concat(resBody) : resBody;
+                })
+              )
+              .subscribe((concatRes: ICustomerDetails[]) => (this.customerids = concatRes));
+          }
+        });
     });
   }
 
-  updateForm(customerDetails: ICustomerDetails): void {
+  updateForm(sales: ISales): void {
     this.editForm.patchValue({
-      id: customerDetails.id,
-      customerName: customerDetails.customerName,
-      email: customerDetails.email,
-      address: customerDetails.address,
-      phone: customerDetails.phone,
+      id: sales.id,
+      total: sales.total,
+      serviceCharges: sales.serviceCharges,
+      dateOfSale: sales.dateOfSale,
+      customerID: sales.customerID,
     });
   }
 
@@ -53,29 +79,26 @@ export class CustomerDetailsUpdateComponent implements OnInit {
 
   save(): void {
     this.isSaving = true;
-    const customerDetails = this.createFromForm();
-    if (customerDetails.id !== undefined) {
-      this.subscribeToSaveResponse(this.customerDetailsService.update(customerDetails));
+    const sales = this.createFromForm();
+    if (sales.id !== undefined) {
+      this.subscribeToSaveResponse(this.salesService.update(sales));
     } else {
-      this.subscribeToSaveResponse(this.customerDetailsService.create(customerDetails));
+      this.subscribeToSaveResponse(this.salesService.create(sales));
     }
   }
 
-  private createFromForm(): ICustomerDetails {
+  private createFromForm(): ISales {
     return {
-      ...new CustomerDetails(),
-      customerName: this.editForm.get(['customerName'])!.value,
-      email: this.editForm.get(['email'])!.value,
-      address: this.editForm.get(['address'])!.value,
-      phone: this.editForm.get(['phone'])!.value,
+      ...new Sales(),
+      id: this.editForm.get(['id'])!.value,
+      total: this.editForm.get(['total'])!.value,
+      serviceCharges: this.editForm.get(['serviceCharges'])!.value,
+      dateOfSale: this.editForm.get(['dateOfSale'])!.value,
+      customerID: this.editForm.get(['customerID'])!.value,
     };
   }
 
-  createCustomer():void{
-    this.salesData.customer = this.createFromForm();
-  }
-
-  public subscribeToSaveResponse(result: Observable<HttpResponse<ICustomerDetails>>): void {
+  public subscribeToSaveResponse(result: Observable<HttpResponse<ISales>>): void {
     result.subscribe(
       () => this.onSaveSuccess(),
       () => this.onSaveError()
@@ -89,5 +112,9 @@ export class CustomerDetailsUpdateComponent implements OnInit {
 
   protected onSaveError(): void {
     this.isSaving = false;
+  }
+
+  trackById(index: number, item: ICustomerDetails): any {
+    return item.id;
   }
 }
